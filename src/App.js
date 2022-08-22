09d862236c61add80a00ff7b2726b4aa67b0f21e
Leonardo Rodrigues <leo-feitosa@live.com>
2022-08-22 17:46:45 -0300

    Parametros de Componentes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto />
+      <CampoTexto label="Nome" placeholder = "Digite o seu nome"/>
+      <CampoTexto label="Cargo" placeholder = "Digite o seu cargo"/>
+      <CampoTexto label="Imagem" placeholder = "Digite o endereço da imagem"/>
+      <CampoTexto label="Time" placeholder = "Digite o seu time"/>
     </div>
   );
 }
