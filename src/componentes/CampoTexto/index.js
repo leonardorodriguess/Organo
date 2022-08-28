bf36d6afa774d96fa6dfba87cb634e7056355074
Leonardo Rodrigues <leo-feitosa@live.com>
2022-08-28 16:46:53 -0300

    Manipulando o estado do componetne

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,12 +1,20 @@
 import './CampoText.css'
 
 const CampoTexto = (props) =>{
-    const placeholder = `${props.placeholder} ...`
+
+    const placeholder = `${props.placeholder} ...`;
+
+    let valor = 'Leonardo';
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value;
+        console.log(valor)
+    }
 
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholder}/>
         </div>
     )
 }
