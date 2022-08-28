c13ae39f994ce5db4b193cc146408dcfc2c1e303
Leonardo Rodrigues <leo-feitosa@live.com>
2022-08-28 15:53:29 -0300

    Criado Botão

---
+++ src/componentes/Botao/index.js
@@ -0,0 +1,10 @@
+import './Botao.css'
+
+const Botao = (props) => {
+    return(<button className='botao'>
+        {/*props.texto*/}
+        {props.children}
+    </button>)
+}
+
+export default Botao
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 
 const Formulario = () => {
 
@@ -24,6 +25,9 @@ const Formulario = () => {
                 <CampoTexto label="Imagem" placeholder = "Digite o endereço da imagem"/>
                 <CampoTexto label="Time" placeholder = "Digite o seu time"/>
                 <ListaSuspensa label = "Time" itens = {times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
