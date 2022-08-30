c1fa890030d96ecdc9d96a8408a7f44ecdea3505
Leonardo Rodrigues <leo-feitosa@live.com>
2022-08-30 18:01:42 -0300

    Elevando a lista de times

--- src/App.js
+++ src/App.js
@@ -5,7 +5,43 @@ import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 
 function App() {
-
+  const times = [
+  {
+    nome: 'Programação',
+    corPrimaria: '#57C278',
+    corSecundaria: '#D9F7E9'
+  },
+  {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+  },
+  {
+      nome: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+  },
+  {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+  },
+  {
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE95F5',
+  },
+  {
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+  },
+  {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+  }
+]
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -19,13 +55,15 @@ function App() {
       <Formulario 
         aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}
       />
-      <Time nome="Programação"/>
-      <Time nome="Frontend"/>
-      <Time nome="Data Science"/>
-      <Time nome="Devops"/>
-      <Time nome="UX Design"/>
-      <Time nome="Mobile"/>
-      <Time nome="Inovação"/>
+
+      {times.map(time => 
+        <Time 
+          key={time.nome} 
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+        />
+      )}
 
 
     </div>
