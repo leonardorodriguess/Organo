3eb0d9a22af96a843eb116eb2b8b1cfe7caa2f0d
Leonardo Rodrigues <leo-feitosa@live.com>
2022-08-30 18:33:06 -0300

    Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/leonardorodriguess.png' alt='Leonardo'/>
+            </div>
+            <div className='rodape'>
+                <h4>Leonardo Rodrigues</h4>
+                <h5>Programador</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
