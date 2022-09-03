f0ae6a15fba1412c6aca8486a223f2db524e8b2b
Leonardo Rodrigues <leo-feitosa@live.com>
2022-09-03 16:23:24 -0300

    adicionando cor no fundo do card

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,7 +3,11 @@ import './Time.css'
 
 const Time = (props) => {
     return(
-        props.colaboradores.length > 0 && <section className='time' style={{backgroundColor : props.corSecundaria}}>
+        props.colaboradores.length > 0 && 
+        <section 
+            className='time'
+            style={{backgroundColor : props.corSecundaria}}>
+                
             <h3 style={{borderColor : props.corPrimaria}}>{props.nome}</h3>
             
             <div className='colaboradores'>
@@ -12,6 +16,7 @@ const Time = (props) => {
                     nome = {colaborador.nome}
                     cargo = {colaborador.cargo}
                     imagem = {colaborador.imagem}
+                    corDeFundo = {props.corPrimaria}
                 />)}
 
             </div>
